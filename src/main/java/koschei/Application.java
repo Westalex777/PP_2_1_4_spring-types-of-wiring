e1926ce5333d8d26046d48c9230755481f377bad
Westalex777 <westalex777@yandex.ru>
2023-01-03 23:46:30 +0600

    commit

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -4,8 +4,14 @@ import koschei.config.AppConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * «На море на океане есть остров, на том острове дуб стоит, под дубом сундук зарыт, в сундуке — заяц,
+ * в зайце — утка, в утке — яйцо, в яйце — игла, — смерть Кощея»
+ */
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("На свете есть океан , на океане остров , на острове дерево , на дереве заяц , в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле :(");
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
